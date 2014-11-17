e9eaf7c96ba8b48bfbffca0a6749785b61decdf4
Kajetan Maj <kajetan.maj@capgemini.com>
2014-11-17 16:12:44 +0100

    Refactor

--- spring-capgemini-core/src/main/java/pl/altkom/spring/spring/capgemini/core/Customer.java
+++ spring-capgemini-core/src/main/java/pl/altkom/spring/spring/capgemini/core/Customer.java
@@ -12,4 +12,14 @@ package pl.altkom.spring.spring.capgemini.core;
  */
 public class Customer {
     
+    private ShoppingCart shoppingCart;
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+    
 }
