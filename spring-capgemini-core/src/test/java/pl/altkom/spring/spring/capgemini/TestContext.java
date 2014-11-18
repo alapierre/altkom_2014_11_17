a6a33f34a47dda2a68a8084abda6817dd631a14f
Adrian Lapierre <adrian@soft-project.pl>
2014-11-18 09:52:06 +0100

    tst kontekstu

---
+++ spring-capgemini-core/src/test/java/pl/altkom/spring/spring/capgemini/TestContext.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.altkom.spring.spring.capgemini;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+/**
+ *
+ * @author instruktor
+ */
+@ContextConfiguration("/core-test-context.xml")
+public class TestContext extends AbstractTransactionalJUnit4SpringContextTests{
+    
+    @Test
+    public void test() {
+        System.out.println("OK");
+    }
+    
+}
