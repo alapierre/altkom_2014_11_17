d7ea4bec1ec76272aadb4496463e6e815d9be26d
Adrian Lapierre <adrian@soft-project.pl>
2014-11-19 10:47:16 +0100

    JAXB i XML

--- spring-capgemini-web/src/main/java/pl/altkom/spring/capgemini/web/controller/MyController.java
+++ spring-capgemini-web/src/main/java/pl/altkom/spring/capgemini/web/controller/MyController.java
@@ -5,9 +5,13 @@ package pl.altkom.spring.capgemini.web.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import pl.altkom.spring.capgemini.web.model.Person;
 
 /**
  *
@@ -28,6 +32,24 @@ public class MyController {
         return model;
     }
     
+    @RequestMapping("/person/show/{id}")
+    public String personShow(Model model, @PathVariable int id) {
+        
+        logger.debug("MyController.personShow()");
+        
+        model.addAttribute("id", -1234);
+        
+        return "person";
+    }
     
+    @RequestMapping(value = "/person/{id}")
+    public @ResponseBody Person person(Model model, @PathVariable int id) {
+        
+        logger.debug("MyController.person()");
+        
+        Person person = new Person("Jan", "Kowąlski");
+        
+        return person;
+    }
     
 }
