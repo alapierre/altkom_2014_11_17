035b1b9c77adeef10bc9f5b979dca35967ad70f0
kursant8 <kursant8@10.12.11.62>
2014-11-20 11:49:43 +0100

    poprawka w pom

--- spring-capgemini-core/src/main/java/pl/altkom/spring/spring/capgemini/core/dao/springdata/OrderDAO.java
+++ spring-capgemini-core/src/main/java/pl/altkom/spring/spring/capgemini/core/dao/springdata/OrderDAO.java
@@ -8,7 +8,6 @@ package pl.altkom.spring.spring.capgemini.core.dao.springdata;
 
 import java.util.List;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
